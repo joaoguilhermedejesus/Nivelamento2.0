b75f7c44486861bd06a605a12bbeb3e1759159e1
joaoguilhermedejesus <105368350+joaoguilhermedejesus@users.noreply.github.com>
2024-03-21 08:32:44 -0300

    s

--- main.js
+++ main.js
@@ -12,5 +12,5 @@ for(let i=0; i <botoes.length; i++) {
     botoes[i].classList.add("ativo");
 }
 }
-contadores[0].textContent = "tempoObjetivo";
 const tempoObjetivo1 = new Date("2024-08-17");
+contadores[0].textContent = "tempoObjetivo";
