657bcd5befb5960c49002a6e615eca3d3f57051d
joaoguilhermedejesus <105368350+joaoguilhermedejesus@users.noreply.github.com>
2024-03-21 08:12:13 -0300

    d

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo")
 
-for(let i=0); i <botoes.length; i++) {
+for(let i=0; i <botoes.length; i++) {
     botoes[i].onclick = function(){
+        for(let j=0;j<botoes.lenght;j++){
+
+       
         botoes[j].classList.remove("ativo");
         botoes[j].classList.remove("ativo");
     }
     botoes[i].classList.add("ativo");
     botoes[i].classList.add("ativo");
+}
 }
