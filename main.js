ca8eb8bb7c6f043fab4fd4e07e786e732ea4a5e3
joaoguilhermedejesus <105368350+joaoguilhermedejesus@users.noreply.github.com>
2024-04-11 08:34:07 -0300

    a

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i = 0; i < botoes.length; i++) {
+for (let i = 0; i <botoes.length; i++) {
     botoes[i].onclick = function () {
 
         for (let j = 0; j < botoes.length; j++) {
@@ -16,9 +16,11 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo4 = new Date("2023-10-05T00:00:00");
 
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempo = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -32,5 +34,10 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
  
+    if (tempoFinal > 0){
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}else{
+        return "Prazo encerrado";
+    }
+    
 }
